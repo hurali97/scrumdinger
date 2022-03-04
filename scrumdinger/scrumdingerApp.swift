3573db996ace475136ea4116e338953d8b2b29ad
Hur Ali <hurali97@gmail.com>
2022-03-04 21:02:16 +0500

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Hur Ali on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
